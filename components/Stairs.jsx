2c997668676bec3043ffb075f43886869e9e992f
EmmanuellDev <emmanuelsk04@gmail.com>
2024-06-19 20:26:10 +0530

    Navbar & its components creation

---
+++ components/Stairs.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { motion } from "framer-motion";
+const stairAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%",
+    },
+    exit: {
+        top: ["100%", "0%"],
+    },
+};
+
+const reverseIndex = (index) => {
+    const totalSteps = 6;
+    return totalSteps - index - 1;
+};
+
+const Stairs = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Stairs
