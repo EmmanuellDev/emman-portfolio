5283370da15dc4cc9adfd3b1cb2e24fe7a4ee234
EmmanuellDev <emmanuelsk04@gmail.com>
2024-06-20 20:10:16 +0530

    Created the home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 const Header = () => {
     return <header className="py-8 xl:py-12 text-white">
         <div className="container mx-auto flex justify-between items-center">
@@ -13,7 +14,9 @@ const Header = () => {
             <Button>Hire Me</Button>
             </Link>
             </div>
-            <div className="xl:hidden">mobile nav</div>
+            <div className="xl:hidden">
+                <MobileNav />
+                </div>
         </div>
         </header>;
 };
